2315349869e162f0e64f010c8e06d50a254fa3e1
bvishal-27 <164020787+bvishal-27@users.noreply.github.com>
2024-07-01 11:08:14 +0000

    datatypes

---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,21 @@
+"use strict"//treat all js code as nr=ewer version
+//alert(3+3)// v r using nodejs, not browser
+console.log(3+3);
+console.log("b vishal");
+//tc39.ec//
+
+ let name="B VISHAL"
+ let age=23
+ let isLogin="false"
+ //bigint
+ //number => 2to power 53
+ //boolean=> true /false
+ //null=> standalone value
+ //undefined
+ //symbol  => uniqune 
+
+ //objects
+
+ // intrview  quesation//
+ console.log(typeof  null); //object
+ console.log(typeof  undefined);// undefined
---
+++ 01_Basics/bv.js
@@ -0,0 +1,7 @@
+let name="B VISHAL"
+const userId=123
+var passWord=123
+state="karnataka"
+let country= "india"
+let village;
+console.table([name,userId,passWord,state,country,village])
