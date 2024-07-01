c8d796dfd480a4f35229aefa183386b098fcc134
bvishal-27 <164020787+bvishal-27@users.noreply.github.com>
2024-07-01 11:44:51 +0000

    conversion

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -7,8 +7,18 @@
 //true=> 1; false=> 0;
 let isLogedIN=1;
 let booleanIsLogedinIn=  Boolean(isLogedIN);
-console.log(booleanIsLogedinIn);
+//console.log(booleanIsLogedinIn);
 //1=> true 0=> false
 // ""=> false
 // bvishal= true
 
+//*******************Operations *************** */
+let value =3
+let negValue= - value
+console.log(negValue)
+//console.log("1"+2+2) 
+//console.log(1+2+"2") 
+
+let gameCounter=100;
+++gameCounter;
+console.log(gameCounter);
