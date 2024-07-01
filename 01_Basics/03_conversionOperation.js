f2e6590da58d52eb9276b2a3b33ebe21bc4fa622
bvishal-27 <164020787+bvishal-27@users.noreply.github.com>
2024-07-01 11:29:10 +0000

    conservision of data types

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,14 @@
+//let score="33"
+//console.log(33)
+//console.log(typeof score);
+//console.log(typeof(score));// method type  writing
+//"33"=>33
+//"33abc"=> NaN
+//true=> 1; false=> 0;
+let isLogedIN=1;
+let booleanIsLogedinIn=  Boolean(isLogedIN);
+console.log(booleanIsLogedinIn);
+//1=> true 0=> false
+// ""=> false
+// bvishal= true
+
