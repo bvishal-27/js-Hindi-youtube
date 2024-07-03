6c5397012c9b43f938630d0a50d744168484b159
B Vishal Bhangare <164020787+bvishal-27@users.noreply.github.com>
2024-07-03 16:33:46 +0000

    datatypes Summaary

--- 01_Basics/dataType-summary.js
+++ 01_Basics/dataType-summary.js
@@ -3,3 +3,45 @@
 
 // Reference type   Non rimitive
 ///. array   objects.  functions
+//******************* DATA TYPE *****************
+//primitive. // call by value
+// 7 types  : strings ,number, boolean , null (empty), undefined. ,symbol. ,BigInt
+
+// Reference type  ( Non rimitive )
+///. array   objects.  functions
+const score=100
+const scoreValue=100.3
+const isLoggedIn=false
+let Email;
+
+
+const id=Symbol("123");
+const anotherId= Symbol("123");
+//console.log(id===anotherId)
+
+
+//BigInt. add last to "n"
+const BigNumber=1224546456767689868777867n
+//console.log("Good Evening 🍀 B VISHAL")
+// Reference type  ( Non rimitive )
+// array   objects.  functions(return type  function)
+
+//Array
+const heros=[ 'Shaktiman','Hanuman'];
+
+//objects
+//key value pairs 👉  key  -> ::::::: value ,,,,,    
+
+let myObj={
+    name :'B Vishal',
+    age : 22,
+}
+
+//function + variable
+const myFunction= function(){
+    console.log("Hello World");
+
+}
+console.log(typeof myFunction)
+
+console.log("Read Screen Shot")
