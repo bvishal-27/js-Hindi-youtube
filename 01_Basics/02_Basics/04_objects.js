bb13db127a1d79b98c171c99af971aa2fea7e7ad
B Vishal Bhangare <164020787+bvishal-27@users.noreply.github.com>
2024-07-06 12:45:19 +0000

    OBject de structuring

--- 01_Basics/02_Basics/04_objects.js
+++ 01_Basics/02_Basics/04_objects.js
@@ -20,7 +20,7 @@ appUser.password="*****"
    const obj2={ 3: "Karnartaka", 4: "Telengana"}
    const obj3={ 5: " Rajasthan", 6: "Punjab"}
    const obj4={...obj1,...obj2,...obj3}
-    console.log(obj4)
+   // console.log(obj4)
 
 
 
@@ -32,4 +32,30 @@ appUser.password="*****"
 //    console.log(Object.keys(tinderUser)); // ->[ 'name', 'id', 'isloggedIn' ]
    
 //  console.log(Object.values(tinderUser));//  -> [ 'Sam', '123svs', false ]
- //   console.log(Object.keys(obj3));
+ //   console.log(Object.keys(obj4));
+
+/* 
+    *
+    * 
+    * 
+    */ 
+
+/*
+ /////////   Object De-Structures     here its syntsx ->const {Indian_Primeir_Leaguage:IPL}=cricket
+ const   cricket ={
+         teams:"8 ",
+          playerd:11,
+          Indian_Primeir_Leaguage: "t20"
+ }
+ const {Indian_Primeir_Leaguage:IPL}=cricket
+ console.log(IPL);
+
+
+ */
+
+ /*.  Json Talk
+{
+   " name" : "B Vishal"
+   "Nationality":"Indian"
+}
+   */
