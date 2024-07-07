6545a3809de8b17723c23f055167f8b267711a25
B Vishal Bhangare <164020787+bvishal-27@users.noreply.github.com>
2024-07-07 18:52:28 +0000

    Functions (Objects +Arrays )

--- 01_Basics/03_Basics/01_functions.js
+++ 01_Basics/03_Basics/01_functions.js
@@ -11,7 +11,7 @@ addTwoNumbers(5,7)
   //   const result= addTwoNumbers(5,7)
    // console.log(result)
 
-
+/*
 
    function loginUser(username = "sam"){
     if(!username){                              //(!usernsme ~ to usernsme===Undefined )
@@ -19,8 +19,49 @@ addTwoNumbers(5,7)
         return
     }
     return `${username} just logged in`
-    
-        
+       
    }
    console.log(loginUser("B Vishal"));
    //console.log(loginUser(""));
+   */
+
+
+  //  *********** Business Purpose **********//
+  //      REST ->        ... parameter like spread to store 2* value
+  function  calculateCarePrice( val1,val2,...num1){
+    return num1
+  }
+ //console.log( calculateCarePrice(200,400,500,800,230,));
+
+
+
+/*
+//. Arrays into functions
+//const myNewArray=[200,600,800,400]
+// creating a function, returning second arrray
+ 
+function returnningSecondValue(getnewArray){
+   return getnewArray[1]
+}
+//console.log(returnningSecondValue( myNewArray));  //-> type 1
+console.log(returnningSecondValue([200,400,600,800])) //-> type 2
+//
+*/
+
+ //   Objects into Functions 
+ const user={
+   user: "B Vishal", 
+   price : "199"
+
+ }
+ function  handleObjects(AnyObjects){
+   console.log(`UserName is ${AnyObjects.user} and price is ${AnyObjects.price}`);
+ }
+ //handleObjects(user)
+
+ handleObjects({
+   user: "sam",
+   price: 999
+ })
+
+
