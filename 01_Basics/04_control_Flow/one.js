75fe1e45cd03067d9175eb90c62726c17cca111e
B Vishal Bhangare <164020787+bvishal-27@users.noreply.github.com>
2024-07-09 17:47:09 +0000

    contro flow

---
+++ 01_Basics/04_control_Flow/one.js
@@ -0,0 +1,9 @@
+//if
+if(2=="2"){
+    console.log ("executed" ) ;
+}
+const temprature=41;
+if(temprature<50){
+    console.log("temp is less than 50");
+}else{
+console.log("temp is greater than 50");}
