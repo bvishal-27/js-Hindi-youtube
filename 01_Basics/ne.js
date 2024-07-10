cf01017ed27a11fc62bd0022b717d351b5b053e8
B Vishal Bhangare <164020787+bvishal-27@users.noreply.github.com>
2024-07-10 17:31:43 +0000

    done

--- 01_Basics/ne.js
+++ 01_Basics/ne.js
@@ -1,1 +1,10 @@
-console.log("Hello")
+
+let useOne={
+    email: "one@gamil",
+    upi:" 123@"
+     }
+     let userTwo= useOne;
+     userTwo.email="bv123@"
+     console.log(userTwo.email)
+     console.log(userOne.email)
+        
