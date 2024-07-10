22b6940b31ed6d06acf9e908a3a726a081905ddb
B Vishal Bhangare <164020787+bvishal-27@users.noreply.github.com>
2024-07-10 17:20:49 +0000

    example

--- 01_Basics/bv.js
+++ 01_Basics/bv.js
@@ -5,3 +5,13 @@ state="karnataka"
 let country= "india"
 let village;
 console.table([name,userId,passWord,state,country,village])
+
+let userOne={
+    email: "one@gamil",
+    upi:" 123@"
+     }
+     let userTwo= userOne
+     userTwo.email="bv123@"
+     console.log(userTwo.email);
+     console.log(userOne.email);
+        
