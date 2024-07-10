8467c34160895312f7d48ad9259a918825ab88df
B Vishal Bhangare <164020787+bvishal-27@users.noreply.github.com>
2024-07-10 17:18:00 +0000

    switch

---
+++ 01_Basics/04_control_Flow/02_switch.js
@@ -0,0 +1,54 @@
+//switch (key) {
+//    case value:
+//       
+//     break;
+//
+//   default:
+//      break;
+//.  }
+
+
+
+ // where ever  case match   afteer all case executed  when we not use break except default
+const  month=3
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("Feb");
+        break;
+    case 3:
+        console.log("mar");
+        break;
+    case 4:
+        console.log("apr");
+        break;
+    case 5:
+        console.log("may ");
+        break;
+
+    default:
+        console.log("default  case match");
+        break;
+}
+
+
+const  months="march"
+switch (months) {
+    case "January":
+        console.log("January");
+        break;
+    case "Feb":
+        console.log("Feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+        case "April":
+        console.log("April");
+        break;
+        default:
+        console.log("please select proper ");
+        break;
+}
