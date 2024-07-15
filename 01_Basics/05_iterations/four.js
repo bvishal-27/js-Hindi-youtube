634e1146512d294fe62dd2e154fba588bbd43465
B Vishal Bhangare <164020787+bvishal-27@users.noreply.github.com>
2024-07-15 17:37:31 +0000

    for in

---
+++ 01_Basics/05_iterations/four.js
@@ -0,0 +1,40 @@
+const myObject={
+    ka: "KANNADA",
+    eng : "English",
+    sp: "spaanish"
+}
+for (const key in myObject) {
+ //   console.log(key); 
+    }
+    /*
+///output :=> ka
+eng
+sp
+*/
+ 
+  //. for in return keys
+const programming=['java','py', 'js','c']
+for (const key in programming) {
+  // console.log(key);
+      /*
+      output
+      1
+2
+3
+*/ 
+    }
+
+
+const program1=['java','py', 'js','c']
+for (const key in program1) {
+   // console.log(program1[key]); // its syntax for value
+        
+    }
+ //output 
+   /*
+java
+py
+js
+c
+*/
+
