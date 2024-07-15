91b3564d98271a3fcae01522ca97fd34b171f67c
B Vishal Bhangare <164020787+bvishal-27@users.noreply.github.com>
2024-07-15 16:27:19 +0000

    while loop

---
+++ 01_Basics/05_iterations/two.js
@@ -0,0 +1,27 @@
+//while loop
+ let index=0;
+ while(index<=10){
+    //console.log(`Value of index is ${index}`);
+    index=index+2;
+ }
+
+
+ //. Array in while loop
+
+let myAray=["apple","ball", "cat"]
+
+ let arr=0;
+ while(arr<myAray.length){
+   // console.log(`Value of ${myAray[arr]}`);
+    arr=arr+1;
+ }
+
+ //do while
+ let score=11;
+ do{
+    console.log(`score is ${score}`);
+    score++;
+ }
+ while(score<=10);
+
+ 
