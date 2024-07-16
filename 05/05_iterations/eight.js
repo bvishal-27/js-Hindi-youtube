05402ee784795510c7bd1ff38e3572a98a38e98c
B Vishal Bhangare <164020787+bvishal-27@users.noreply.github.com>
2024-07-16 16:44:51 +0000

    JS Basics

---
+++ 05/05_iterations/eight.js
@@ -0,0 +1,12 @@
+//reduce
+ const price=[1,2,3,4]
+ const initial=0
+ const sumWithInitial=price.reduce((accu,curr)=>accu +curr,initial);
+ console.log(sumWithInitial);
+ //const myNums = [1, 2, 3]
+ //const myTotal = myNums.reduce(function (acc, currval) {
+ //console.log(`acc: ${acc} and currval: ${currval}`); 
+ //return acc  + currval
+//},0)
+
+//const myTotal = myNums.reduce ((acc, curr)=acc+curr,0)
